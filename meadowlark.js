316e8f20273c319b3d9c0175cb5a4384faa73b20
HwasooSung <hwasu92@naver.com>
2016-04-15 15:35:54 +0900

    this is new commit

--- meadowlark.js
+++ meadowlark.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fortune = require('./lib/fortune');
 
 var app = express();
 
@@ -8,26 +9,30 @@ var handlebars = require('express-handlebars')
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-app.set('port', process.env.PORT || 3000);
 
+app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
-var fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple."
-];
+app.use(function (req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
 
+// route
 app.get('/', function (req, res) {
     res.render('home');
 });
 app.get('/about', function (req, res) {
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+    res.render('about', { 
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js' });
 });
-
+app.get('/tours/hood-river', function(req, res) {
+   res.render('tours/hood-river'); 
+});
+app.get('/tours/request-group-rate', function(req, res) {
+   res.render('tours/request-group-rate'); 
+});
 // 커스텀 404 페이지
 app.use(function (req, res) {
     res.status(404);
@@ -42,7 +47,5 @@ app.use(function (err, req, res, next) {
 });
 
 app.listen(app.get('port'), function () {
-console.log('Express started on http://localhost:' 
-            + app.get('port') 
-            + '; press Ctrl-c to terminate');
+console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-c to terminate');
 });
---
+++ public/qa/tests-about.js
@@ -0,0 +1,5 @@
+suite('"About test" Page Tests', function () {
+    test('page should contain link to contact page', function () {
+        assert($('a[href="/contact"]').length);
+    });
+});
